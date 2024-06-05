7b74138ab20c457df960ac0a688b39b46ffbd40b
Will <willen.garcia05@gmail.com>
2024-06-05 08:57:49 -0300

    Começando a utilizar o js

---
+++ desafio_Formulario/script.js
@@ -0,0 +1,6 @@
+const inputEmail = document.getElementById('email')
+const botaoSubmit = document.getElementById('submit')
+
+botaoSubmit.addEventListener('click', ()=>{
+    window.location.assign('/alert.html')
+})
