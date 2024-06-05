ea3951e2191046441208185c11e4f1d73936425c
Will <willen.garcia05@gmail.com>
2024-06-05 10:42:18 -0300

    Finalização do desafio Formulário

--- desafio_Formulario/script.js
+++ desafio_Formulario/script.js
@@ -1,6 +1,18 @@
 const inputEmail = document.getElementById('email')
 const botaoSubmit = document.getElementById('submit')
+const spanErro = document.querySelector('span')
 
 botaoSubmit.addEventListener('click', ()=>{
-    window.location.assign('/alert.html')
+    let formulario = document.forms['minhaValidacao']['email'].value
+    let regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(formulario == '' || regex.test(formulario) == false){
+        inputEmail.classList.toggle('erro')
+        spanErro.classList.toggle('erro')
+        console.log('não validado')
+        return false
+    }else {
+        localStorage.setItem('transferindoValor', formulario)
+        window.location.href= 'alert.html'
+        return true
+    }
 })
