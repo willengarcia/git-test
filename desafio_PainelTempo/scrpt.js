bae6c7b526a300938ee3cfb29b3959f1420f9a0e
Willen Garcia <89990936+willengarcia@users.noreply.github.com>
2024-07-15 22:41:24 -0300

    sla

--- desafio_PainelTempo/scrpt.js
+++ desafio_PainelTempo/scrpt.js
@@ -1,12 +1,24 @@
-fetch('./data.json').then((request)=>{
-    if(!request.ok) {
-        console.log('Algo deu errado')
-        return null
-    }
-    return request.json()
-}).then((data)=>{
-    console.log(data);
-    return data.forEach((el)=>{
-        console.log(el)
+const botoes = [...document.querySelectorAll('.botoes')]
+const p = [...document.querySelectorAll('.previsao')]
+const h3 = [...document.querySelectorAll('.horas')]
+
+let work = p[0]
+let play = p[1]
+let study = p[2]
+let exercice = p[3]
+let social = p[4]
+let self = p[5]
+
+
+botoes.forEach((el)=>{
+    el.addEventListener('click', (evt)=>{
+        fetch('./data.json')
+        .then(res=>res.json())
+        .then(res=>{
+            const botao = evt.target.innerHTML
+            if(botao.toUpperCase() == res[0].title.toUpperCase()){
+                console.log('oi')
+            }
+        })
     })
 })
